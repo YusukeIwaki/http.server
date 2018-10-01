2d03c40f05e0db20dad080e66f7f82e3e1848f5a
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-10-01 23:21:36 +0900

    enable builds for windows.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gox -osarch="darwin/amd64 linux/386 linux/amd64" -output="./out/bin/http.server_{{.OS}}_{{.Arch}}" -parallel=2
+gox -osarch="darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -output="./out/bin/http.server_{{.OS}}_{{.Arch}}" -parallel=2

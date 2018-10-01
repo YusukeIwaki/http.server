87697735ba6ce2a51d49d56945689e84dc7d3cc4
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-10-01 23:56:11 +0900

    add logging.

--- main.go
+++ main.go
@@ -5,7 +5,17 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
+
+type LoggingHandler struct {
+	Handler http.Handler
+}
+
+func (loggingHandler *LoggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("%s\t%s\t\t\"%s %s\"\n", time.Now().Format(time.RFC3339), r.RemoteAddr, r.Method, r.URL.Path)
+    loggingHandler.Handler.ServeHTTP(w, r)
+}
 
 func main() {
 	port := "8080"
@@ -15,7 +25,8 @@ func main() {
 	}
 	if err == nil {
 		fmt.Printf("Serving HTTP on 0.0.0.0 port %s ...\n", port)
-		err = http.ListenAndServe(":"+port, http.FileServer(http.Dir(dir)))
+		handler := LoggingHandler { http.FileServer(http.Dir(dir)) }
+		err = http.ListenAndServe(":"+port, &handler)
 	}
 	panic(err)
 }
